6d30a5303133d1f2188c24962a3c04e96d3daf21
SlavikusVOG <slavaolish@gmail.com>
2022-05-22 17:54:05 +0300

    feat: implement task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+import { mkdir, readdir, readFile, writeFile } from "fs/promises";
+import path from "node:path";
 
+const copyDir = async (currDir) => {
+	await mkdir(path.join(currDir, 'files-copy'), {recursive: true});
+	const files = await readdir(path.join(currDir, 'files'), {withFileTypes: true});
+	files.forEach(async(file) => {
+		const fileContent = await readFile(
+			path.join(currDir, 'files', file.name)
+		);
+		await writeFile(
+			path.join(path.join(currDir, 'files-copy'), file.name),
+			fileContent
+		);
+	});
+}
+
+const currentDirectory = path.join(".", '04-copy-directory');
+
+copyDir(currentDirectory);
