d2ca862960a2aaea23e7c395c0f41ba98c8c3643
SlavikusVOG <slavaolish@gmail.com>
2022-05-23 19:37:39 +0300

    feat: implement task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,29 +1,69 @@
-import { cp, mkdir, readdir, readFile, writeFile } from "fs/promises";
+import { copyFile, cp, mkdir, readdir, readFile, writeFile } from "fs/promises";
 import path from 'path';
 
 const currentDir = path.join('.', '06-build-page');
 const projectDirName = 'project-dist';
+const componentsDirName = 'components';
+const assetsDirName = 'assets';
 const re = /{{\w+}}/gm;
+
+async function copyDir(currDir, destDir, copyDirName){
+	try {
+		await mkdir(path.join(destDir, copyDirName), {recursive: true});
+	} catch(err) {/* folders created*/}
+	const files = await readdir(path.join(currDir, copyDirName), {withFileTypes: true});
+	files.forEach(async(file) => {
+		if (file.isDirectory()) {
+			copyDir(
+				path.join(currDir, copyDirName),
+				path.join(destDir, copyDirName),
+				file.name
+			);
+		}
+		else {
+			// const fileContent = await readFile(
+			// 	path.join(currDir, copyDirName, file.name)
+			// );
+			// await writeFile(
+			// 	path.join(path.join(destDir, copyDirName), file.name),
+			// 	fileContent
+			// );
+			try{
+				await copyFile(
+					path.join(currDir, copyDirName, file.name),
+					path.join(destDir, copyDirName, file.name)
+				);
+			} catch (err) {
+				console.error(err.message);
+			}
+		}
+	});
+}
 
 try {
 	await mkdir(path.join(currentDir, projectDirName));
-	await mkdir(path.join(currentDir, projectDirName, 'assets'));
+	await mkdir(path.join(currentDir, projectDirName, assetsDirName));
 } catch(err) {/* folders created */}
-await cp(path.join(currentDir, 'assets'), path.join(currentDir, projectDirName, 'assets'));
+await copyDir(currentDir, path.join(currentDir, projectDirName), assetsDirName);
 
-const templateContent = await readFile(path.join(currentDir, 'template.html'));
+const templateContent = await readFile(path.join(currentDir, 'template.html'), {encoding: 'utf-8'});
 const sections = templateContent.match(re);
-sections.map(section => section.substr(2, -2)).forEach(async (sectionName) => {
-	const sectionContent = await readFile(path.join(currentDir, 'components', `${sectionName}.html`), {encoding: 'utf-8'});
-	templateContent.replace(`{{${sectionName}}}`, sectionContent);
-});
-await writeFile(path.join(currentDir, projectDirName, 'index.html'));
+const sectionNames = sections.map(section => section.slice(2, -2));
+const contents = await Promise.all(
+	sectionNames.map(async (sectionName) => {
+		return readFile(path.join(currentDir, componentsDirName, `${sectionName}.html`), {encoding: 'utf-8'});
+	})
+);
+const htmlContent = sections.reduce((content, section, index) => {
+	return content.replace(section, contents[index]);
+}, templateContent);
+await writeFile(path.join(currentDir, projectDirName, 'index.html'), htmlContent);
 
-const cssFiles = readdir(path.join(currentDir, 'styles'), {withFileTypes: true});
+const cssFiles = await readdir(path.join(currentDir, 'styles'), {withFileTypes: true});
 const cssFilesDir = path.join(currentDir, 'styles');
 const bundleContent = await Promise.all(cssFiles.map(async (file) => {
 	if(path.extname(path.join(cssFilesDir, file.name)) === '.css') {
-		const fileContent = await readFile(path.join(sourceDir, file.name), {encoding: 'utf-8'});
+		const fileContent = await readFile(path.join(cssFilesDir, file.name), {encoding: 'utf-8'});
 		return fileContent;
 	} else {
 		return '';
