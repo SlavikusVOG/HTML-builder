f33d7c08f05d36a4365ce64060f0624c33016b08
SlavikusVOG <slavaolish@gmail.com>
2022-05-26 00:09:35 +0300

    fix: update task 3 according requirements

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,14 +5,16 @@ const getFilesInfo = async (filesPath) => {
   const content = await readdir(filesPath, {withFileTypes: true});
   const filesInfo = await Promise.all(content.map(async (obj) => {
     if(obj.isDirectory()) {
-      return getFilesInfo(path.join(filesPath, obj.name));
+      // TODO: use recursion if you want to see files in subfolders too.
+      // return getFilesInfo(path.join(filesPath, obj.name));
+    } else {
+      const fileStat = await stat(path.join(filesPath, obj.name));
+      return {
+        name: obj.name.indexOf('.') !== 0 ? obj.name.slice(0, obj.name.lastIndexOf('.')) : obj.name,
+        ext: path.extname(path.join(filesPath, obj.name)).slice(1),
+        size: `${fileStat.size} B`
+      };
     }
-    const fileStat = await stat(path.join(filesPath, obj.name));
-    return {
-      name: obj.name.indexOf('.') !== 0 ? obj.name.slice(0, obj.name.lastIndexOf('.')) : obj.name,
-      ext: path.extname(path.join(filesPath, obj.name)).slice(1),
-      size: `${fileStat.size} B`
-    };
   }));
   const result = [];
   filesInfo.forEach((info) => {
@@ -29,9 +31,12 @@ const dirPath = path.join('.', '03-files-in-folder', '/secret-folder');
 
 try {
   const files = await getFilesInfo(dirPath);
-  const filesInfo = files.map((file) => {
-    return `${file.name} - ${file.ext} - ${file.size}`;
-  });
+  const filesInfo = files.reduce((info, file) => {
+	  if (file) {
+		  info.push(`${file.name} - ${file.ext} - ${file.size}`)
+	  }
+	  return info;
+  }, []);
   filesInfo.forEach((info) => {
     console.log(info);
   })
